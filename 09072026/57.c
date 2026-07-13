cc0db99b5f9aa53fb3fd923941f459fb65161471
Anandarraman V M <anandarraman1999@gmail.com>
2026-07-13 16:08:44 +0530

    Delete 09072026 directory

--- 09072026/57.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-int a;
-char *x;
-printf("Enter the Number :");
-scanf("%d",&a);
-x=(a%5==0 && a%7==0)?"Divisible by both":(a%5==0?"Divisible by 5":(a%7==0? "divisible by 7":"not by both"));
-printf("%s",x);
-}
--- 09072026/circle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float a;
-    int b;
-    printf("Enter the radius of the circle:");
-    scanf("%f",&a);
-    b=2*3.14f*a;
-    printf("The cerc. of the circle is : %d",b);
-}
